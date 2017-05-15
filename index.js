52d3cc5fe73612f6e9734353a837b807739db55a
Esteban Calvi <estebancalvi@gmail.com>
2017-05-15 12:17:19 -0300

    New config

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const hbs  = require('hbs')
+const port = 3000
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (request, response) => {
+  return response.render('index');
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
+
+app.get('/', (request, response) => {
+  throw new Error('oops')
+})
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+hbs.registerPartials(__dirname + '/views')
+hbs.registerPartials(__dirname + '/views/layouts')
